c6745041536790f8e811c16c049a223e459298a7
CallMeThunder <t3hunderteicker@gmail.com>
2023-01-31 23:55:10 +0100

    Fix Scroll

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ document.addEventListener('DOMContentLoaded',domloaded,false);
 let cells = [[1,5],[2,5],[1,6],[2,6],[11,5],[11,6],[11,7],[12,4],[12,8],[13,3],[13,9],[14,3],[14,9],[15,6],[16,4],[16,8],[17,5],[17,6],[17,7],[18,6],[21,3],[21,4],[21,5],[22,3],[22,4],[22,5],[23,2],[23,6],[25,1],[25,2],[25,6],[25,7],[35,3],[36,3],[35,4],[36,4]];
 let bg = "#222222";
 let cellColor = "#FFFFFF";
-let ar = window.innerHeight/window.innerWidth;
 let cellSize;
 let cellScreenWidth = 45;
 let ctx;
